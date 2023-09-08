edafc9842ff1ff0d54cb677aa31949140aed8ad4
Mark Caneda <us66@klavenessdigital.com>
2023-09-08 20:52:10 +0800

    Linkedin Spider component test

--- tests/test_jobs_unified_spider.py
+++ tests/test_jobs_unified_spider.py
@@ -3,6 +3,7 @@ import requests
 import json
 
 from jobs_scraper.spiders.indeed_flare_scraper import IndeedFlareSpider
+from jobs_scraper.spiders.linkedin_spider import LinkedInSpider
 from jobs_unified.jobs_scraper.jobs_scraper.spiders.jobstreet_spider import JobStreetSpider
 from scrapy.http.response.html import HtmlResponse
 
@@ -49,6 +50,9 @@ indeed_item_keys = ['base_url',
                     'date']
 
 
+linkedin_item_keys = ['seniority_level', 'employment_type', 'job_function', 'job_industries', 'url', 'project', 'spider', 'server', 'date']
+
+
 @pytest.mark.parametrize(
     "url,expected",
     [("https://www.jobstreet.com.ph/jobs?pg=1",  jobstreet_item_keys)]
@@ -93,3 +97,28 @@ def test_indeed_json_response_did_not_changed(url, expected):
         job_card_response = requests.post(url=r.url, headers=flare_headers, json=post_body)
         indeed_item: dict = next(spider.parse_job_card(response=HtmlResponse(url=response_body["url"], body=job_card_response.text, encoding="utf-8")))
         assert list(indeed_item.keys()) == expected
+
+
+@pytest.mark.parametrize(
+    "url,expected",
+    [("https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=&location=Philippines&geoId=103121230&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0&start=0",  linkedin_item_keys)]
+)
+def test_linkedin_html_jobs_structure_did_not_changed(url, expected):
+    flare_base_url = "http://localhost:8191/v1"
+    flare_headers = {'Content-Type': 'application/json'}
+
+    spider = LinkedInSpider(settings={"BOT_NAME": "test_spider"})
+    post_body = {'cmd': 'request.get', 'url': url, 'maxTimeout': 60000}
+    job_list_response = requests.post(flare_base_url, headers=flare_headers, json=post_body)
+    scrapy_response = HtmlResponse(url=url, body=job_list_response.json()["solution"]["response"], encoding="utf-8")
+
+    result = list(spider.parse(scrapy_response, page_num=0))
+
+    # 1 job page has 26 jobs
+    assert len(result) == 26
+
+    for r in result:
+        post_body = {'cmd': 'request.get', 'url': r.url, 'maxTimeout': 60000}
+        job_card_response = requests.post(url=r.url, headers=flare_headers, json=post_body)
+        linkedin_item: dict = next(spider.parse_job_item(response=HtmlResponse(url=r.url, body=job_card_response.text, encoding="utf-8")))
+        assert list(linkedin_item.keys()) == expected
