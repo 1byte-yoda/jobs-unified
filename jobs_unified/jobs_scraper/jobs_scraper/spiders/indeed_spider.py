0aeb32b631fe42cc04243de7390e51e4c283dbc2
Mark Caneda <us66@klavenessdigital.com>
2023-09-08 16:33:24 +0800

    Removed total page calculation and used a fix value of 700

--- jobs_unified/jobs_scraper/jobs_scraper/spiders/indeed_spider.py
+++ jobs_unified/jobs_scraper/jobs_scraper/spiders/indeed_spider.py
@@ -23,40 +23,23 @@ class IndeedSpider(scrapy.Spider):
     URL = "https://ph.indeed.com/jobs?filter=0&q=all&l=Philippines&pp=gQAAAAAAAAAAAAAAAAACD-M2ugADAAABAAA"
     name = "indeed"
 
-    @staticmethod
-    def get_html_page(url):
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
-        driver = webdriver.Chrome(options=chrome_options, service=Service(executable_path=binary_path))
-        driver.get(url)
-
-        return driver.page_source
-
-    @classmethod
-    def get_total_pages(cls) -> int:
-        jobs_per_pages = 15
-        html_body = cls.get_html_page(url=cls.URL)
-        response = HtmlResponse(url=cls.URL, body=html_body, encoding="utf-8")
-        total_jobs = response.xpath("//div[contains(@class, 'jobCount')]//span//text()").extract_first()
-        total_jobs = int(total_jobs.strip(" jobs").replace(",", ""))
-        return 1 + (total_jobs // jobs_per_pages)
-
     def start_requests(self):
-        total_pages = self.get_total_pages()
 
-        urls = [f"{self.URL}&start={i}" for i in range(0, total_pages + 10, 10)]
+        urls = [f"{self.URL}&start={i}" for i in range(0, 700, 10)]
         for url in urls:
             yield SeleniumRequest(url=url, callback=self.parse, wait_time=random.randint(1, 4))
 
     def parse(self, response: HtmlResponse, **kwargs):
         job_links = response.xpath("//a[contains(@class, 'jcs-JobTitle') and contains(@id, 'job_')]/@href").extract()
-        indeed_view_job_url = "https://ph.indeed.com"
+        indeed_view_job_url = "https://ph.indeed.com/viewjob?jk="
 
         for url_path in job_links:
-            req_url = f"{indeed_view_job_url}{url_path}"
+            if url_path.startswith("/rc/"):
+                jk = re.findall(r"jk=(.+)&.+", url_path)[0].split("&")[0]
+            else:
+                jk = url_path.split("?")[0].split("-")[-1]
 
-            yield SeleniumRequest(url=req_url, callback=self.parse_job_card, wait_time=random.randint(1, 4))
+            yield SeleniumRequest(url=f"{indeed_view_job_url}{jk}", callback=self.parse_job_card, wait_time=random.randint(1, 4))
 
     def parse_job_card(self, response: HtmlResponse):
         data = re.findall(r'window._initialData=(\{.+?\});', response.text)
