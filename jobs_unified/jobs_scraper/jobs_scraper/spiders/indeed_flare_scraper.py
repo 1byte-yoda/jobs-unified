7bbf1f555120ff8baefdb2dd19b5e49233323861
Mark Caneda <us66@klavenessdigital.com>
2023-09-08 17:07:06 +0800

    Improved logging and re-introduced total pages calculation for pagination

--- jobs_unified/jobs_scraper/jobs_scraper/spiders/indeed_flare_scraper.py
+++ jobs_unified/jobs_scraper/jobs_scraper/spiders/indeed_flare_scraper.py
@@ -1,7 +1,10 @@
 import datetime
 import json
+import logging
 import re
 import socket
+
+import requests
 import scrapy
 from itemloaders.processors import TakeFirst
 from scrapy.loader import ItemLoader
@@ -10,24 +13,45 @@ from scrapy.http.request.json_request import JsonRequest
 
 from jobs_scraper.items import IndeedItem
 
+logger = logging.getLogger("scrapy.core.scraper")
+
 
 class IndeedFlareSpider(scrapy.Spider):
-    URL = "https://ph.indeed.com/jobs?filter=0&q=all&l=Philippines"
+    URL = "https://ph.indeed.com/jobs?filter=0&q=all&l=Philippines&sort=date"
     name = "indeed_flare"
 
     flare_base_url = "http://localhost:8191/v1"
     flare_headers = {'Content-Type': 'application/json'}
 
+    @classmethod
+    def get_total_pages(cls):
+        post_body = {'cmd': 'request.get', 'url': cls.URL, 'maxTimeout': 60000}
+        response = requests.post(cls.flare_base_url, headers=cls.flare_headers, json=post_body)
+        html_response = HtmlResponse(url=cls.URL, body=response.json()["solution"]["response"], encoding="utf-8")
+        total_jobs = html_response.xpath("//*[contains(@class, 'jobCount')]/span/text()").extract_first()
+
+        jobs_per_page = 15
+        margin_offset = 10
+        total_pages = margin_offset + (int(total_jobs.split(" ")[0].replace(",", "")) // jobs_per_page)
+
+        logger.debug(f"There are {total_jobs} jobs to be scraped within {total_pages} pages")
+
+        return total_pages
+
     def start_requests(self):
 
-        urls = [f"{self.URL}&start={i}" for i in range(0, 700, 10)]
+        urls = [f"{self.URL}&start={i}" for i in range(0, self.get_total_pages(), 10)]
 
         for url in urls:
             post_body = {'cmd': 'request.get', 'url': url, 'maxTimeout': 60000}
             yield JsonRequest(url=self.flare_base_url, headers=self.flare_headers, data=post_body, callback=self.parse)
 
     def parse(self, response: HtmlResponse, **kwargs):
-        response = HtmlResponse(url=response.json()["solution"]["url"], body=response.json()["solution"]["response"], encoding="utf-8")
+        json_response = response.json()
+
+        logger.debug(f"Scraped from <{json_response['solution']['status']}{json_response['solution']['url']}>")
+
+        response = HtmlResponse(url=json_response["solution"]["url"], body=json_response["solution"]["response"], encoding="utf-8")
 
         job_keys = response.xpath("//a[contains(@class, 'jcs-JobTitle')]/@id").extract()
         indeed_view_job_url = "https://ph.indeed.com/viewjob?jk="
@@ -38,7 +62,11 @@ class IndeedFlareSpider(scrapy.Spider):
             yield JsonRequest(url=self.flare_base_url, headers=self.flare_headers, data=post_body, callback=self.parse_job_card)
 
     def parse_job_card(self, response: HtmlResponse):
-        response = HtmlResponse(url=response.json()["solution"]["url"], body=response.json()["solution"]["response"], encoding="utf-8")
+        json_response = response.json()
+
+        logger.debug(f"Scraped from <{json_response['solution']['status']}{json_response['solution']['url']}>")
+
+        response = HtmlResponse(url=json_response["solution"]["url"], body=json_response["solution"]["response"], encoding="utf-8")
         data = re.findall(r'window._initialData=(\{.+?\});', response.text)
         # data = re.findall(r'(\{"accountKey".+"jobInfoWrapperModel".*\});', response.text)
 
@@ -60,6 +88,9 @@ class IndeedFlareSpider(scrapy.Spider):
             loader.add_value("locale", json_response.get("locale"))
             loader.add_value("request_path", json_response.get("requestPath"))
             loader.add_value("salary_info_model", json.dumps(json_response.get("salaryInfoModel", {})))
+
+        else:
+            logger.warning(r"HTML Response might have changed, 'window._initialData=(\{.+?\}' didn't matched a JSON data")
 
         loader.add_value(field_name="url", value=response.url)
         loader.add_value(field_name="project", value=self.settings.get("BOT_NAME"))
